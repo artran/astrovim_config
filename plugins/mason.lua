918a9f93faf974d7aca31e7dc03b3ed0fb74db54
Ray Tran <rt@elixirsoftware.co.uk>
2023-07-23 22:26:19 +0100

    Add ansible-lint

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -7,8 +7,13 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        "lua_ls", "ansiblels", "docker_compose_language_service", "dockerls", "rust_analyzer",
-        "terraformls", "tflint",
+        "lua_ls",
+        "ansiblels",
+        "docker_compose_language_service",
+        "dockerls",
+        "rust_analyzer",
+        "terraformls",
+        "tflint",
       })
     end,
   },
@@ -20,6 +25,7 @@ return {
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
         -- "prettier",
+        "ansible-lint",
         "stylua",
       })
     end,
