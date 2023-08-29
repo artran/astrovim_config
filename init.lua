61bdfd4eecfb236f157ba4c84108389e0730e7a0
Ray Tran <rt@elixirsoftware.co.uk>
2023-08-29 16:47:50 +0100

    Ensure pre-tasks also get detected as ansible

--- init.lua
+++ init.lua
@@ -75,7 +75,7 @@ return {
       if type(content) == "table" then content = table.concat(content, "\n") end
 
       -- check if file is in roles, tasks, or handlers folder
-      local path_regex = vim.regex "(tasks\\|roles\\|handlers)/"
+      local path_regex = vim.regex "(pre-tasks\\|tasks\\|roles\\|handlers)/"
       if path_regex and path_regex:match_str(path) then return "yaml.ansible" end
       -- check for known ansible playbook text and if found, return yaml.ansible
       local regex = vim.regex "hosts:\\|tasks:"
