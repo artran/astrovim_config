293496bf9f97af6a3c4939653dae997df2655699
Ray Tran <rt@elixirsoftware.co.uk>
2024-02-03 14:58:39 +0000

    Move copilot config to community

--- plugins/community.lua
+++ plugins/community.lua
@@ -3,5 +3,6 @@ return {
 
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
   { import = "astrocommunity.utility.noice-nvim" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.completion.copilot-lua" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
