72e3780da5a84b7d2521c21ef23356bfbb83dec3
Ray Tran <rt@elixirsoftware.co.uk>
2024-02-03 14:53:24 +0000

    Add Noice to nvim config

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,9 +1,7 @@
 return {
-  -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
-  -- example of imporing a plugin, comment out to use it or add your own
-  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+  { import = "astrocommunity.utility.noice-nvim" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
