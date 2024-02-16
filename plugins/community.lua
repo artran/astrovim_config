88b06ae171c80105ed2b1d4d00b15f96ad62d05f
Ray Tran <rt@elixirsoftware.co.uk>
2024-02-16 17:00:33 +0000

    Move json support to community plugin

--- plugins/community.lua
+++ plugins/community.lua
@@ -10,6 +10,7 @@ return {
   { import = "astrocommunity.pack.ansible" },
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.python" },
