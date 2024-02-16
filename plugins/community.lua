1d10f6f620f6f210e31832c9b605f8a4c306a97c
Ray Tran <rt@elixirsoftware.co.uk>
2024-02-16 11:02:26 +0000

    Add markdown support

--- plugins/community.lua
+++ plugins/community.lua
@@ -7,6 +7,7 @@ return {
   { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.editing-support.vim-move" },
   { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.terraform" },
