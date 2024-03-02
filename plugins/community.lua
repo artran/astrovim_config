c1fca93428de9a03af39ea1f09a0c2396fd33f04
Ray Tran <rt@elixirsoftware.co.uk>
2024-03-02 12:12:09 +0000

    Add golang support

--- plugins/community.lua
+++ plugins/community.lua
@@ -12,6 +12,7 @@ return {
   { import = "astrocommunity.pack.ansible" },
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.go" },
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.markdown" },
