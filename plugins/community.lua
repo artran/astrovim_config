41d7d39b15c52e1eee129f9eee7816b3313213bc
Ray Tran <rt@elixirsoftware.co.uk>
2024-03-19 13:10:45 +0000

    Change NixOS support to just have the TreeSitter

--- plugins/community.lua
+++ plugins/community.lua
@@ -16,7 +16,6 @@ return {
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.markdown" },
-  { import = "astrocommunity.pack.nix" },
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.terraform" },
